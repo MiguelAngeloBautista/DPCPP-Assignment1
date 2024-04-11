cdfb6236685d7a15cd6ec81da7a383a3c07bfb2a
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2024-04-12 00:55:26 +0930

    80% implementation of Game class

--- Assigment1/Game.cpp
+++ Assigment1/Game.cpp
@@ -1,28 +1,98 @@
 ﻿#include "Game.h"
 
 Game::Game() {
-    // ItemManager itemManager
-    // MooonManager moonManager
-    // SimulatorManager simulatorManager
-    // RandomNumberGenerator rng
+    // ItemManager itemManager = new ItemManager();
+    
+    // SimulatorManager simulatorManager;
+    // RandomNumberGenerator rng = new RandomNumberGenerator();
+    
+    moonManager = new MoonManager();
+    moonManager->registerMoon(new Corporation());
+    moonManager->registerMoon(new Prototyping());
+    moonManager->registerMoon(new Insurance());
+    moonManager->registerMoon(new Pledge());
+    moonManager->registerMoon(new Defense());
 
     balance = 0;
-    // AbstractMoon* currentMoon
-    currentPhase;
-    currentDay = 0;
+    currentMoon = moonManager->moons().at("Corporation"); // new Corporation();
+    currentPhase = ORBIT;
+    currentDay = 1;
     cargoValue = 0;
     aliveEmployees = 4;
+    quota = 150;
+
+    
+
+}
+
+Game::~Game() {
+    delete moonManager;
+
 }
 
 
 void Game::land() {
     // Call the moon's onDayBegin
+    currentPhase = LAND;
+    // currentMoon->onDayBegin(*this);
+
+    // if (getAliveEmployees == 0) {
+    
+    // }
 }
 
 void Game::leave() {
-
+    moonManager->regenerateWeather();
+    currentPhase = ORBIT;
+    currentDay += 1;
 }
 
 void Game::exit() {
+    currentMoon = nullptr;
+    /*delete itemManager;
+    delete rng;*/
 
+}
+
+int Game::getAliveEmployees() {
+    return aliveEmployees;
+}
+
+void Game::setAliveEmployees(int num) {
+    aliveEmployees = num;
+}
+
+// Add Cargo Value by n Amount. Convert to negative to subtract
+void Game::addCargoValue(int num) {
+    cargoValue += num;
+}
+
+int Game::getCargoValue() {
+    return cargoValue;
+}
+
+int Game::getQuota() {
+    return quota;
+}
+
+// Add balance by n Amount. Convert to negative to subtract
+void Game::addBalance(int num) {
+    balance += num;
+}
+
+int Game::getBalance() {
+    return balance;
+}
+
+int Game::getCurrentDay() {
+    return currentDay;
+}
+
+AbstractMoon* Game::getCurrentMoon() {
+    return currentMoon;
+}
+
+ 
+phases Game::getCurrentPhase() const {
+    return currentPhase;
 }
--- Assigment1/Game.h
+++ Assigment1/Game.h
@@ -1,38 +1,43 @@
 ﻿#ifndef GAME_H
 #define GAME_H
 #include <iostream>
-#include "Game.h"
+//#include "Moons/AbstractMoon.h"
+#include "Moons/Corporation.h"
+#include "Moons/Prototyping.h"
+#include "Moons/Insurance.h"
+#include "Moons/Pledge.h"
+#include "Moons/Defense.h"
+#include "Moons/MoonManager.h"
+
+static enum phases { ORBIT, LAND };
 
 class Game {
+public:
+	
+	Game();
+	virtual ~Game();
+	void land();
+	void leave();
+	void exit();
+	int getAliveEmployees();
+	void setAliveEmployees(int num);
+	void addCargoValue(int num);
+	int getCargoValue();
+	int getQuota();
+	void addBalance(int num);
+	int getBalance();
+	int getCurrentDay();
+	AbstractMoon* getCurrentMoon();
+	phases getCurrentPhase() const;
 private:
-	// ItemManager itemManager
-	// MooonManager moonManager
-	// SimulatorManager simulatorManager
-	// RandomNumberGenerator rng
-
+	MoonManager* moonManager;
 	int balance;
-	// AbstractMoon* currentMoon
-	std::string currentPhase;
+	AbstractMoon* currentMoon;
+	phases currentPhase;
 	int currentDay;
 	int cargoValue;
 	int aliveEmployees;
-
-
-public:
-	Game();
-	virtual ~Game();
-
-	void land() {
-
-	}
-
-	void leave() {
-
-	}
-
-	void exit() {
-
-	}
+	int quota;
 };
 
 #endif // GAME_H
